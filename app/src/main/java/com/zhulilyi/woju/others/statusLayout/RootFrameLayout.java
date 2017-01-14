9117229ff4a9885795783c8dc07dd5cf11a64960
zhuliyi10 <2280835569@qq.com>
2017-01-14 13:47:02 +0800

    第一次提交

---
+++ app/src/main/java/com/zhulilyi/woju/others/statusLayout/RootFrameLayout.java
@@ -0,0 +1,212 @@
+package com.zhulilyi.woju.others.statusLayout;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.LayoutRes;
+import android.util.AttributeSet;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.FrameLayout;
+
+/**
+ * Created by chenpengfei on 2016/12/15.
+ */
+public class RootFrameLayout extends FrameLayout {
+
+    /**
+     *  loading 加载id
+     */
+    public static final int LAYOUT_LOADING_ID = 1;
+
+    /**
+     *  内容id
+     */
+    public static final int LAYOUT_CONTENT_ID = 2;
+
+    /**
+     *  异常id
+     */
+    public static final int LAYOUT_ERROR_ID = 3;
+
+    /**
+     *  网络异常id
+     */
+    public static final int LAYOUT_NETWORK_ERROR_ID = 4;
+
+    /**
+     *  空数据id
+     */
+    public static final int LAYOUT_EMPTYDATA_ID = 5;
+
+    /**
+     *  存放布局集合
+     */
+    private SparseArray<View> layoutSparseArray = new SparseArray();
+
+    /**
+     *  网络布局
+     */
+    private ViewStub netWorkErrorVs;
+
+    /**
+     * 空数据布局
+     */
+    private ViewStub emptyDataVs;
+
+    /**
+     * 异常布局
+     */
+    private ViewStub errorVs;
+
+    private OnShowHideListener mOnShowHideListener;
+
+
+    public RootFrameLayout(Context context) {
+        super(context);
+    }
+
+    public RootFrameLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RootFrameLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public RootFrameLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public void addLayoutResId(Context context, @LayoutRes int layoutResId, int id) {
+        View resView = LayoutInflater.from(context).inflate(layoutResId, null);
+        layoutSparseArray.put(id, resView);
+        addView(resView);
+    }
+
+    public void addViewStub(ViewStub viewStub, int id) {
+        switch (id) {
+            case LAYOUT_NETWORK_ERROR_ID:
+                netWorkErrorVs = viewStub;
+                addView(netWorkErrorVs);
+                break;
+            case LAYOUT_ERROR_ID:
+                errorVs = viewStub;
+                addView(errorVs);
+                break;
+            case LAYOUT_EMPTYDATA_ID:
+                emptyDataVs = viewStub;
+                addView(emptyDataVs);
+                break;
+        }
+    }
+
+    /**
+     *  显示loading
+     */
+    public void showLoading() {
+        if(layoutSparseArray.get(LAYOUT_LOADING_ID) != null)
+            showHideViewById(LAYOUT_LOADING_ID);
+    }
+
+    /**
+     *  显示内容
+     */
+    public void showContent() {
+        if(layoutSparseArray.get(LAYOUT_CONTENT_ID) != null)
+            showHideViewById(LAYOUT_CONTENT_ID);
+    }
+
+    /**
+     *  显示空数据
+     */
+    public void showEmptyData() {
+        if(inflateLayout(LAYOUT_EMPTYDATA_ID))
+            showHideViewById(LAYOUT_EMPTYDATA_ID);
+    }
+
+    /**
+     *  显示网络异常
+     */
+    public void showNetWorkError() {
+        if(inflateLayout(LAYOUT_NETWORK_ERROR_ID))
+            showHideViewById(LAYOUT_NETWORK_ERROR_ID);
+    }
+
+    /**
+     *  显示异常
+     */
+    public void showError() {
+        if(inflateLayout(LAYOUT_ERROR_ID))
+            showHideViewById(LAYOUT_ERROR_ID);
+    }
+
+    /**
+     *  根据ID显示隐藏布局
+     * @param id
+     */
+    private void showHideViewById(int id) {
+        for(int i = 0; i < layoutSparseArray.size(); i++) {
+            int key = layoutSparseArray.keyAt(i);
+            View valueView = layoutSparseArray.valueAt(i);
+            //显示该view
+            if(key == id) {
+                valueView.setVisibility(View.VISIBLE);
+                if(mOnShowHideListener != null) mOnShowHideListener.onShow(valueView, key);
+            } else {
+                if(valueView.getVisibility() != View.GONE) {
+                    valueView.setVisibility(View.GONE);
+                    if(mOnShowHideListener != null) mOnShowHideListener.onHide(valueView, key);
+                }
+            }
+        }
+    }
+
+    private boolean inflateLayout(int id) {
+        boolean isShow = true;
+        if(layoutSparseArray.get(id) != null) return isShow;
+        switch (id) {
+            case LAYOUT_NETWORK_ERROR_ID:
+                if(netWorkErrorVs != null) {
+                    View view = netWorkErrorVs.inflate();
+                    layoutSparseArray.put(id, view);
+                    isShow = true;
+                } else {
+                    isShow = false;
+                }
+                break;
+            case LAYOUT_ERROR_ID:
+                if(errorVs != null) {
+                    View view = errorVs.inflate();
+                    layoutSparseArray.put(id, view);
+                    isShow = true;
+                } else {
+                    isShow = false;
+                }
+                break;
+            case LAYOUT_EMPTYDATA_ID:
+                if(emptyDataVs != null) {
+                    View view = emptyDataVs.inflate();
+                    layoutSparseArray.put(id, view);
+                    isShow = true;
+                } else {
+                    isShow = false;
+                }
+                break;
+        }
+        return isShow;
+    }
+
+    public void setOnShowHideListener(OnShowHideListener onShowHideListener) {
+        mOnShowHideListener = onShowHideListener;
+    }
+
+    interface OnShowHideListener {
+        void onShow(View view, int id);
+        void onHide(View view, int id);
+    }
+}
+
---
+++ app/src/main/java/com/zhulilyi/woju/others/statusLayout/StatusLayoutManager.java
@@ -0,0 +1,165 @@
+package com.zhulilyi.woju.others.statusLayout;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewStub;
+
+/**
+ * Created by chenpengfei on 2016/12/15.
+ */
+public class StatusLayoutManager {
+
+    private final Context context;
+    private final ViewStub netWorkErrorVs;
+    private final ViewStub emptyDataVs;
+    private final ViewStub errorVs;
+    private final int loadingLayoutResId;
+    private final int contentLayoutResId;
+
+    private final RootFrameLayout rootFrameLayout;
+    private final OnShowHideViewListener onShowHideViewListener;
+
+    public StatusLayoutManager(Builder builder) {
+        this.context = builder.context;
+        this.loadingLayoutResId = builder.loadingLayoutResId;
+        this.netWorkErrorVs = builder.netWorkErrorVs;
+        this.emptyDataVs = builder.emptyDataVs;
+        this.errorVs = builder.errorVs;
+        this.contentLayoutResId = builder.contentLayoutResId;
+        this.onShowHideViewListener = builder.onShowHideViewListener;
+
+        rootFrameLayout = new RootFrameLayout(this.context);
+        rootFrameLayout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
+        addAllLayoutToRootLayout();
+        addShowHideListener();
+    }
+
+    /**
+     *  所有局部view添加到根布局
+     */
+    private void addAllLayoutToRootLayout() {
+        if(this.contentLayoutResId != 0) rootFrameLayout.addLayoutResId(context, contentLayoutResId, RootFrameLayout.LAYOUT_CONTENT_ID);
+        if(this.loadingLayoutResId != 0) rootFrameLayout.addLayoutResId(context, loadingLayoutResId, RootFrameLayout.LAYOUT_LOADING_ID);
+
+        if(this.emptyDataVs != null) rootFrameLayout.addViewStub(emptyDataVs, RootFrameLayout.LAYOUT_EMPTYDATA_ID);
+        if(this.errorVs != null) rootFrameLayout.addViewStub(errorVs, RootFrameLayout.LAYOUT_ERROR_ID);
+        if(this.netWorkErrorVs != null) rootFrameLayout.addViewStub(netWorkErrorVs, RootFrameLayout.LAYOUT_NETWORK_ERROR_ID);
+    }
+
+    private void addShowHideListener() {
+        rootFrameLayout.setOnShowHideListener(new RootFrameLayout.OnShowHideListener() {
+            @Override
+            public void onShow(View view, int id) {
+                if(onShowHideViewListener != null) onShowHideViewListener.onShowView(view, id);
+            }
+
+            @Override
+            public void onHide(View view, int id) {
+                if(onShowHideViewListener != null) onShowHideViewListener.onHideView(view, id);
+            }
+        });
+    }
+
+    /**
+     *  显示loading
+     */
+    public void showLoading() {
+        rootFrameLayout.showLoading();
+    }
+
+    /**
+     *  显示内容
+     */
+    public void showContent() {
+        rootFrameLayout.showContent();
+    }
+
+    /**
+     *  显示空数据
+     */
+    public void showEmptyData() {
+        rootFrameLayout.showEmptyData();
+    }
+
+    /**
+     *  显示网络异常
+     */
+    public void showNetWorkError() {
+        rootFrameLayout.showNetWorkError();
+    }
+
+    /**
+     *  显示异常
+     */
+    public void showError() {
+        rootFrameLayout.showError();
+    }
+
+    /**
+     *  得到root 布局
+     */
+    public View getRootLayout() {
+        return rootFrameLayout;
+    }
+
+
+    public static final class Builder {
+
+        private Context context;
+        private int loadingLayoutResId;
+        private int contentLayoutResId;
+        private ViewStub netWorkErrorVs;
+        private ViewStub emptyDataVs;
+        private ViewStub errorVs;
+        private OnShowHideViewListener onShowHideViewListener;
+
+        public Builder(Context context) {
+            this.context = context;
+        }
+
+        public Builder loadingView(@LayoutRes int loadingLayoutResId) {
+            this.loadingLayoutResId = loadingLayoutResId;
+            return this;
+        }
+
+        public Builder netWorkErrorView(@LayoutRes int newWorkErrorId) {
+            netWorkErrorVs = new ViewStub(context);
+            netWorkErrorVs.setLayoutResource(newWorkErrorId);
+            return this;
+        }
+
+        public Builder emptyDataView(@LayoutRes int noDataViewId) {
+            emptyDataVs = new ViewStub(context);
+            emptyDataVs.setLayoutResource(noDataViewId);
+            return this;
+        }
+
+        public Builder errorView(@LayoutRes int errorViewId) {
+            errorVs = new ViewStub(context);
+            errorVs.setLayoutResource(errorViewId);
+            return this;
+        }
+
+        public Builder contentView(@LayoutRes int contentLayoutResId) {
+            this.contentLayoutResId = contentLayoutResId;
+            return this;
+        }
+
+        public Builder onShowHideViewListener(OnShowHideViewListener onShowHideViewListener) {
+            this.onShowHideViewListener = onShowHideViewListener;
+            return this;
+        }
+
+        public StatusLayoutManager build() {
+            return new StatusLayoutManager(this);
+        }
+    }
+
+    public static Builder newBuilder(Context context) {
+       return new Builder(context);
+    }
+
+}
